6b80a2424f04b7a7c7b9403a3c88bf5e9d161ac6
Illina Burlace <chloeburlace@gmail.com>
2026-03-07 22:06:57 +0000

    removed entity combination

--- common/src/main/kotlin/meow/illina/hexssence/registry/HexssenceActions.kt
+++ common/src/main/kotlin/meow/illina/hexssence/registry/HexssenceActions.kt
@@ -6,7 +6,6 @@ import at.petrak.hexcasting.api.casting.math.HexDir
 import at.petrak.hexcasting.api.casting.math.HexPattern
 import at.petrak.hexcasting.common.lib.HexRegistries
 import at.petrak.hexcasting.common.lib.hex.HexActions
-import meow.illina.hexssence.casting.actions.spells.CombineEntities
 import meow.illina.hexssence.casting.actions.spells.CombineVecs
 import meow.illina.hexssence.casting.actions.spells.Extract
 
@@ -14,10 +13,9 @@ object HexssenceActions : HexssenceRegistrar<ActionRegistryEntry>(
     HexRegistries.ACTION,
     { HexActions.REGISTRY },
 ) {
-    val EXTRACT = make("extract_essence", HexDir.EAST, "qaqqaeawaeaeawqwa", Extract)
+    val EXTRACT = make("extract_essence", HexDir.SOUTH_WEST, "qqqqwaadw", Extract)
 
-    val COMBINE_ENTITIES = make("combine_entities", HexDir.NORTH_WEST, "edewdwdwwdwd", CombineEntities)
-    val COMBINE_JARS = make("combine_jars", HexDir.EAST, "awddwdewedqeqde", CombineVecs)
+    val COMBINE = make("combine_essence", HexDir.SOUTH_EAST, "eeeewddaw", CombineVecs)
 
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
