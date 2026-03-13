2357c8e19fff1de00aff144299683af18e94451c
Illina Burlace <chloeburlace@gmail.com>
2026-03-13 14:30:02 +0000

    changed IngredientCounted to just rely on ingredient tag (why was it not doing this before??)

--- common/src/main/kotlin/meow/illina/hexssence/recipe/utils/IngredientCounted.kt
+++ common/src/main/kotlin/meow/illina/hexssence/recipe/utils/IngredientCounted.kt
@@ -2,41 +2,14 @@ package meow.illina.hexssence.recipe.utils
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
-import net.minecraft.core.registries.Registries
 import net.minecraft.network.FriendlyByteBuf
-import net.minecraft.tags.TagKey
 import net.minecraft.util.GsonHelper
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 
-class IngredientCounted {
-    var ingredient: Ingredient? = null
-    var tag: TagKey<Item>? = null
-    var count = 1
-
-    constructor(ingredient: Ingredient) {
-        this.ingredient = ingredient
-    }
-    constructor(ingredient: Ingredient, count: Int) {
-        this.ingredient = ingredient
-        this.count = count
-    }
-    constructor(tag: TagKey<Item>) {
-        this.tag = tag
-    }
-    constructor(tag: TagKey<Item>, count: Int) {
-        this.tag = tag
-        this.count = count
-    }
-
-    fun test(item: Item): Boolean {
-        return(test(ItemStack(item)))
-    }
-
+class IngredientCounted(val ingredient: Ingredient, val count: Int) {
     fun test(item: ItemStack): Boolean {
-        if (tag == null) return ingredient!!.test(item)
-        return item.`is`(tag!!)
+        return ingredient.test(item)
     }
 
     fun testWithCount(item: ItemStack): Boolean {
@@ -58,23 +31,11 @@ class IngredientCounted {
         }
 
         fun fromNetwork(buf: FriendlyByteBuf): IngredientCounted {
-            if (!buf.readBoolean()) {
-                return IngredientCounted(Ingredient.fromNetwork(buf), buf.readInt())
-            }
-            return IngredientCounted(
-                TagKey.create(Registries.ITEM, buf.readResourceLocation()),
-                buf.readInt()
-            )
+            return IngredientCounted(Ingredient.fromNetwork(buf), buf.readInt())
         }
 
         fun toNetwork(buf: FriendlyByteBuf, ingredient: IngredientCounted) {
-            buf.writeBoolean(ingredient.tag == null)
-            if (ingredient.tag == null)
-                ingredient.ingredient!!.toNetwork(buf)
-            else {
-                val tag = ingredient.tag!!
-                buf.writeResourceLocation(tag.location)
-            }
+            ingredient.ingredient.toNetwork(buf)
             buf.writeInt(ingredient.count)
         }
     }
