14caf3c2812bb731c0effd201f2b4070d44a8331
Illina Burlace <chloeburlace@gmail.com>
2026-03-17 20:09:27 +0000

    Decay now returns [[itemtype, num]]

--- common/src/main/kotlin/meow/illina/hexssence/casting/actions/spells/Decay.kt
+++ common/src/main/kotlin/meow/illina/hexssence/casting/actions/spells/Decay.kt
@@ -2,12 +2,13 @@ package meow.illina.hexssence.casting.actions.spells
 
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.iota.DoubleIota
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.ListIota
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import meow.illina.hexssence.casting.iota.RecipeIota
 import meow.illina.hexssence.recipe.combination.CombinationRecipe
-import ram.talia.moreiotas.api.casting.iota.StringIota
+import ram.talia.moreiotas.api.casting.iota.ItemTypeIota
 
 object Decay : ConstMediaAction {
     override val argc = 1
@@ -21,27 +22,16 @@ object Decay : ConstMediaAction {
             .orElseThrow { MishapInvalidIota.ofType(iota, 0, "Recipe") }
         as CombinationRecipe
 
-        val coll = mutableListOf<StringIota>()
+        val coll = mutableListOf<ListIota>()
 
         recipe.params.ingredients.forEach { ing ->
-            if (ing.ingredient.items.size == 1) {
-                val item = ing.ingredient.items[0]
-                coll.add(StringIota.make(
-                    item.item.`arch$registryName`().toString()
-                    + " x"
-                    + ing.count.toString()
-                ))
-            }
-            else {
-                var str = "["
-                ing.ingredient.items.forEach { i ->
-                    str += i.item.`arch$registryName`().toString()
-                    str += ", "
-                }
-                str.removeSuffix(", ")
-                str += "] x" + ing.count.toString()
-                coll.add(StringIota.make(str))
-            }
+            val item = ing.ingredient.items[0]
+            coll.add(ListIota(
+                listOf(
+                    ItemTypeIota(item.item),
+                    DoubleIota(ing.count.toDouble())
+                )
+            ))
         }
 
         return listOf(ListIota(coll as List<Iota>))
